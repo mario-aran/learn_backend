6a2be9ebe72115fec3464aa7376cafbb14e2572f
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-10 11:18:12 -0300

    index schemas

--- src/libs/drizzle/schemas/index.ts
+++ src/libs/drizzle/schemas/index.ts
@@ -1,4 +1,6 @@
+export * from './clients-to-sales.schema';
 export * from './clients.schema';
 export * from './product-categories.schema';
+export * from './products-to-sales.schema';
 export * from './products.schema';
 export * from './sales.schema';
