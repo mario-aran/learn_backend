0c973231ddb1d33cf0cbb61706809bc4ba1cb3bc
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-13 16:34:06 -0300

    retrieving data

---
+++ lessons/01-retrieving-data.sql
@@ -0,0 +1,26 @@
+/* Retrieving Data */
+
+-- `SELECT FROM`
+SELECT 1, 2, 3; -- One or multiple values separated by commas
+SELECT column FROM table_name; -- Column
+SELECT * FROM table_name; -- * wildcard that means all columns
+SELECT 1, column, * FROM table_name; -- Combination of number, column and all columns
+
+-- `SELECT DISTINCT`
+SELECT DISTINCT column1, column2 FROM table_name; -- Select distinct values
+
+-- Arithmetic Operators: `+`, `-`, `*`, `/`, `%`, `()`
+SELECT 1 + 1;
+SELECT 1 - 1;
+SELECT 1 * 1;
+SELECT 1 / 1;
+SELECT 1 % 1;
+SELECT 2 * (1 + 1); -- P. E. M. D. A. S.
+
+-- Aliases: `AS`
+/*
+- Use "" to avoid errors on reserved keywords
+- `AS` is optional 
+*/
+SELECT column AS "alias"; -- Column alias
+SELECT column FROM table_name AS "tn"; -- Table alias
