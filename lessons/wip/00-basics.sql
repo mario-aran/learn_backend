dd243f0f14daa8a405fc1ac1da1f25d1122d6d49
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-13 17:43:07 -0300

    basics

---
+++ lessons/wip/00-basics.sql
@@ -0,0 +1,38 @@
+/* Basics */
+
+-- Concepts
+- SQL: -- (Structured Query Language), allows to interact with a database.
+- DB: -- Data organizade and structured in multiple tables.
+- Tables: -- Matrix of records, like excel sheets.
+- Columns: -- Fields of a table.
+- Keys: -- Fields that uniquely identify a record (PK, FK).
+- Rows: -- Records of a table.
+
+-- Tools
+- RDBMS: -- (Relational Database Management System), PostgreSQL, MySQL, SQL Server, Oracle.
+- Clients: -- "DBeaver", "SQL Server Management Studio", "MySQL Workbench".
+- DBeaver: -- Installation and settings "https://dbeaver.io/download/"
+    -- Connection: new database connection -> all -> postgresql -> host, port, database, user, password
+    -- Uppercase commands: window -> preferences -> editors ->sql editor -> formatting -> keyword case -> upper
+
+-- Syntax
+- Queries: `SELECT column_name FROM table_name;`
+- Semicolon: `;`
+- Case insensitive
+- UPPERCASE: -- In SQL commands
+- snake_case: -- In name of tables/columns 
+
+-- Data Types
+- Number: 1, 1.1
+- Text: 'This is a string'
+- Date and Time: '2024-12-13 14:30:45'
+- Boolean: true, false
+- Null: NULL
+
+-- Comments
+-- This is a single line comment
+
+/*
+This is a multi-line comment
+in multiple lines
+*/
