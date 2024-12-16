1c388c62aa731d81a5be49e5be5cf1888038adfd
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-16 16:57:51 -0300

    retrieving data

--- lessons/sql/01-retrieving-data.sql
+++ lessons/sql/01-retrieving-data.sql
@@ -1,12 +1,22 @@
 /* Retrieving Data */
 
 /* `SELECT FROM` */
-SELECT * FROM products; -- All columns
-SELECT id FROM products; -- One column
-SELECT *, id, 1 FROM products; -- Multiple values, separated by commas
+-- All columns
+SELECT *
+FROM products;
+
+-- One column
+SELECT id
+FROM products;
+
+-- Multiple values, separated by commas
+SELECT *, id, 1
+FROM products;
 
 /* `SELECT DISTINCT` */
-SELECT DISTINCT product_category_id, price FROM table; -- Unique values
+-- Unique values
+SELECT DISTINCT product_category_id, price
+FROM table;
 
 /* Arithmetic Operators: `+`, `-`, `*`, `/`, `%` */
 SELECT 1 + 1;
@@ -16,7 +26,10 @@ SELECT 1 / 1;
 SELECT 1 % 1;
 SELECT 2 * (1 + 1); -- P. E. M. D. A. S.
 
-/* Aliases: `AS` */
+/* Aliases: `AS`
 -- Use "" to avoid errors on reserved keywords
--- `AS` is optional 
-SELECT price AS "alias" FROM products as "p"; -- Column and table
+-- `AS` is optional
+*/
+-- Column and table
+SELECT price AS "alias"
+FROM products as "p";
