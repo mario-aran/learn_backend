2d634fdc60a9cb64cc3f6af45357693d54f8a2c0
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-16 16:29:31 -0300

    data modification

--- lessons/sql/09-data-modification.sql
+++ lessons/sql/09-data-modification.sql
@@ -1,17 +1,17 @@
 /* Data Modification */
 
-/* INSERT INTO */
+/* `INSERT INTO` */
 -- Adds data to a table
 INSERT INTO products (name, price, product_category_id)
 VALUES ('solaris', 15000, 1), ('anti reflejo', 20000, 2);
 
-/* UPDATE SET */
+/* `UPDATE SET` */
 -- Modifies existing data
 UPDATE products
 SET price = 10000
 WHERE product_category_id = 1;
 
-/* DELETE FROM */
+/* `DELETE FROM` */
 -- Removes records from a table
 DELETE FROM products
 WHERE product_category_id = 1;
