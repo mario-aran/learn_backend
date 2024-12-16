3d966c08dad5f97a47c6d003833619b84e79b4a0
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-16 17:02:36 -0300

    aggregating data

--- lessons/sql/04-aggregating-data.sql
+++ lessons/sql/04-aggregating-data.sql
@@ -1,8 +1,8 @@
 /* Aggregating Data */
 
-/* Aggregate Functions: `COUNT`, `SUM`, `AVG`, `MIN`, `MAX` */
--- Can't be used inside `WHERE`
-
+/* Aggregate Functions: `COUNT`, `SUM`, `AVG`, `MIN`, `MAX`
+- Can't be used inside `WHERE`
+*/
 SELECT COUNT(*) FROM products; -- Count total number of rows, including nulls
 SELECT COUNT(id) FROM products; -- Count non-null values
 SELECT SUM(id) FROM products; -- Sum of all non-null values
@@ -11,7 +11,6 @@ SELECT MIN(id) FROM products; -- Minimum non-null value
 SELECT MAX(id) FROM products; -- Maximum non-null value
 
 /* `GROUP BY` */
-
 -- Column
 SELECT product_category_id
 FROM products
@@ -22,9 +21,9 @@ SELECT COUNT(id), product_category_id
 FROM products
 GROUP BY product_category_id;
 
-/* `HAVING` */
--- To filter aggregate functions
-
+/* `HAVING`
+- To filter aggregate functions
+*/
 -- Single condition
 SELECT AVG(price), product_category_id
 FROM products
