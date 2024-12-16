c267c026b4e319f9c99d0c9bad8385af66b17964
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-16 16:42:33 -0300

    case statements

--- lessons/sql/05-case-statements.sql
+++ lessons/sql/05-case-statements.sql
@@ -8,7 +8,7 @@ SELECT CASE
 END
 
 /* Case in `SELECT` */
-(CASE WHEN price = 1 THEN 1 END) AS "custom_name"; -- Alias and Null as default value
+(CASE WHEN price = 1 THEN 1 END) AS "custom_price"; -- Alias and Null as default value
 CASE WHEN price = 1 THEN 1 ELSE 2 END; -- Default value
 SUM(CASE WHEN price = 1 THEN 1 END); -- Aggregate function
 CASE WHEN (case1) > 1 THEN 2 ELSE (case2) END; -- Nested cases
