d3d022a385160f1e518bc6c2e502968f7469ed6c
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-18 19:03:12 -0300

    orders

--- src/libs/drizzle/schemas/orders.schema.ts
+++ src/libs/drizzle/schemas/orders.schema.ts
@@ -1,29 +1,26 @@
-import { relations } from 'drizzle-orm';
-import { integer, pgTable } from 'drizzle-orm/pg-core';
-import { BASE_COLUMNS } from '../constants';
+import { BASE_DATE_COLUMNS } from '@/libs/drizzle/constants';
+import { pgTable, uuid } from 'drizzle-orm/pg-core';
 import { clientsSchema } from './clients.schema';
-import { productsSchema } from './products.schema';
+import { sellersSchema } from './sellers.schema';
 
-// Constants
-const TABLE_NAME = 'orders';
-
-export const ordersSchema = pgTable(TABLE_NAME, {
-  ...BASE_COLUMNS,
-  clientId: integer('client_id')
+export const ordersSchema = pgTable('orders', {
+  id: uuid().primaryKey().defaultRandom(),
+  sellerId: uuid('seller_id')
+    .notNull()
+    .references(() => sellersSchema.id),
+  clientId: uuid('client_id')
     .notNull()
     .references(() => clientsSchema.id),
-  productId: integer('product_id')
-    .notNull()
-    .references(() => productsSchema.id),
+  ...BASE_DATE_COLUMNS,
 });
 
-export const salesRelations = relations(ordersSchema, ({ one }) => ({
-  client: one(clientsSchema, {
-    fields: [ordersSchema.clientId],
-    references: [clientsSchema.id],
-  }),
-  product: one(productsSchema, {
-    fields: [ordersSchema.productId],
-    references: [productsSchema.id],
-  }),
-}));
+// export const salesRelations = relations(ordersSchema, ({ one }) => ({
+//   client: one(clientsSchema, {
+//     fields: [ordersSchema.clientId],
+//     references: [clientsSchema.id],
+//   }),
+//   product: one(productsSchema, {
+//     fields: [ordersSchema.productId],
+//     references: [productsSchema.id],
+//   }),
+// }));
