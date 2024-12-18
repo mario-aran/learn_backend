574ac296e9b3840f2d986d92ac50b8e635e23336
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-18 19:27:27 -0300

    products

--- src/libs/drizzle/schemas/products.schema.ts
+++ src/libs/drizzle/schemas/products.schema.ts
@@ -1,29 +1,21 @@
-import { relations } from 'drizzle-orm';
-import { integer, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
-import { ordersSchema } from './orders.schema';
+import { BASE_DATE_COLUMNS } from '@/libs/drizzle/constants';
+import { decimal, pgTable, uuid, varchar } from 'drizzle-orm/pg-core';
 import { productCategoriesSchema } from './product-categories.schema';
 
-// Constants
-const TABLE_NAME = 'products';
-
-export const productsSchema = pgTable(TABLE_NAME, {
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at')
-    .notNull()
-    .defaultNow()
-    .$onUpdate(() => new Date()),
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar('name').notNull(),
-  price: integer('price').notNull(),
-  productCategoryId: integer('product_category_id')
+export const productsSchema = pgTable('products', {
+  id: uuid().primaryKey().defaultRandom(),
+  productCategoryId: uuid('product_category_id')
     .notNull()
     .references(() => productCategoriesSchema.id),
+  ...BASE_DATE_COLUMNS,
+  name: varchar('name').notNull().unique(),
+  unitPrice: decimal('unit_price', { precision: 10, scale: 2 }).notNull(),
 });
 
-export const productsRelations = relations(productsSchema, ({ one, many }) => ({
-  productCategory: one(productCategoriesSchema, {
-    fields: [productsSchema.productCategoryId],
-    references: [productCategoriesSchema.id],
-  }),
-  orders: many(ordersSchema),
-}));
+// export const productsRelations = relations(productsSchema, ({ one, many }) => ({
+//   productCategory: one(productCategoriesSchema, {
+//     fields: [productsSchema.productCategoryId],
+//     references: [productCategoriesSchema.id],
+//   }),
+//   orders: many(ordersSchema),
+// }));
