24b5e8cb97ef670052bcebbb6ae36b6ec059d184
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-19 18:08:56 -0300

    clients

--- src/libs/drizzle/schemas/client-discounts.schema.ts
+++ src/libs/drizzle/schemas/client-discounts.schema.ts
@@ -1,8 +1,17 @@
 import { BASE_DATE_COLUMNS } from '@/libs/drizzle/constants';
+import { relations } from 'drizzle-orm';
 import { decimal, pgTable, uuid } from 'drizzle-orm/pg-core';
+import { clientsSchema } from './clients.schema';
 
 export const clientDiscountsSchema = pgTable('client_discounts', {
   id: uuid().primaryKey().defaultRandom(),
   ...BASE_DATE_COLUMNS,
   discount: decimal('discount', { precision: 4, scale: 2 }),
 });
+
+export const clientDiscountsRelations = relations(
+  clientDiscountsSchema,
+  ({ many }) => ({
+    clients: many(clientsSchema),
+  }),
+);
--- src/libs/drizzle/schemas/clients.schema.ts
+++ src/libs/drizzle/schemas/clients.schema.ts
@@ -1,20 +1,30 @@
 import { BASE_DATE_COLUMNS } from '@/libs/drizzle/constants';
+import { relations } from 'drizzle-orm';
 import { pgTable, uuid } from 'drizzle-orm/pg-core';
 import { clientDiscountsSchema } from './client-discounts.schema';
+import { ordersSchema } from './orders.schema';
 import { usersSchema } from './users.schema';
 
 export const clientsSchema = pgTable('clients', {
   id: uuid().primaryKey().defaultRandom(),
+  clientDiscountId: uuid('client_discount_id')
+    .notNull()
+    .references(() => clientDiscountsSchema.id),
   userId: uuid('user_id')
     .notNull()
     .unique()
     .references(() => usersSchema.id),
-  clientDiscountId: uuid('client_discount_id')
-    .notNull()
-    .references(() => clientDiscountsSchema.id),
   ...BASE_DATE_COLUMNS,
 });
 
-// export const clientsRelations = relations(clientsSchema, ({ many }) => ({
-//   sales: many(ordersSchema),
-// }));
+export const clientsRelations = relations(clientsSchema, ({ one, many }) => ({
+  orders: many(ordersSchema),
+  clientDiscount: one(clientDiscountsSchema, {
+    fields: [clientsSchema.clientDiscountId],
+    references: [clientDiscountsSchema.id],
+  }),
+  user: one(usersSchema, {
+    fields: [clientsSchema.userId],
+    references: [usersSchema.id],
+  }),
+}));
