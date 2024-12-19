caa10fd2e3900705951616579f48d37a20ab0972
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-19 10:26:39 -0300

    users-to-user-roles

--- src/libs/drizzle/schemas/users-to-user-roles.schema.ts
+++ src/libs/drizzle/schemas/users-to-user-roles.schema.ts
@@ -1,9 +1,18 @@
-import { BASE_COLUMNS } from '@/libs/drizzle/constants/base-columns';
-import { pgTable } from 'drizzle-orm/pg-core';
+import { BASE_DATE_COLUMNS } from '@/libs/drizzle/constants';
+import { pgTable, primaryKey, uuid } from 'drizzle-orm/pg-core';
+import { ordersSchema } from './orders.schema';
+import { productsSchema } from './products.schema';
 
-// Constants
-const TABLE_NAME = 'users_to_user_roles';
-
-export const usersToUserRolesSchema = pgTable(TABLE_NAME, {
-  ...BASE_COLUMNS,
-});
+export const usersToUserRolesSchema = pgTable(
+  'users_to_user_roles',
+  {
+    userId: uuid('user_id')
+      .notNull()
+      .references(() => ordersSchema.id),
+    userRoleId: uuid('user_role_id')
+      .notNull()
+      .references(() => productsSchema.id),
+    ...BASE_DATE_COLUMNS,
+  },
+  (t) => [primaryKey({ columns: [t.userId, t.userRoleId] })],
+);
