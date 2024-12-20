01f8803e87916f178b337c94977f439cdb1e1078
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-19 22:05:16 -0300

    users

--- src/libs/drizzle/schemas/user-roles.schema.ts
+++ src/libs/drizzle/schemas/user-roles.schema.ts
@@ -1,8 +1,14 @@
 import { BASE_DATE_COLUMNS } from '@/libs/drizzle/constants';
+import { relations } from 'drizzle-orm';
 import { pgTable, uuid, varchar } from 'drizzle-orm/pg-core';
+import { usersToUserRolesSchema } from './users-to-user-roles.schema';
 
 export const userRolesSchema = pgTable('user_roles', {
   id: uuid().primaryKey().defaultRandom(),
   ...BASE_DATE_COLUMNS,
   name: varchar('name', { length: 255 }).notNull().unique(),
 });
+
+export const userRolesRelations = relations(userRolesSchema, ({ many }) => ({
+  usersToUserRoles: many(usersToUserRolesSchema),
+}));
