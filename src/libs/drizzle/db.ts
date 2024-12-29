f2bc009237ec658ee7485f9be315ab7948c4ccfc
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-28 22:12:56 -0300

    added schemas to db file to use drizzle query

--- src/libs/drizzle/db.ts
+++ src/libs/drizzle/db.ts
@@ -1,6 +1,8 @@
 import { DATABASE_URL } from '@/config/env';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import * as schemas from './schemas';
 
 export const db = drizzle(DATABASE_URL, {
+  schema: schemas, // All schemas to use in drizzle query
   logger: true, // Logs
 });
