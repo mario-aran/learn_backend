f33c154c470dfe3b232d1b3ae0ec5406a9884679
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-29 10:01:16 -0300

    mocks

--- src/libs/drizzle/seeds/mocks.ts
+++ src/libs/drizzle/seeds/mocks.ts
@@ -1,25 +1,21 @@
 import { faker } from '@faker-js/faker';
 
 // Utils
-const getRandomValue = (arr: string[]) =>
-  arr[Math.floor(Math.random() * arr.length)];
+const getRandomString = (strings: string[]) =>
+  strings[Math.floor(Math.random() * strings.length)];
 
 // Mocks
-export const mockClient = {
-  discount: () =>
-    faker.number.float({ multipleOf: 0.05, min: 0, max: 0.8 }).toString(),
-};
+export const mockClientDiscount = () =>
+  faker.number.float({ multipleOf: 0.05, min: 0, max: 0.8 }).toString();
 
-export const mockProduct = {
-  categoryName: () => getRandomValue(['Basic', 'Medium', 'Premium']),
-  name: () => faker.commerce.productName(),
-  unitPrice: () => faker.commerce.price({ min: 100, max: 1000, dec: 2 }),
-  quantity: () => faker.number.int({ min: 1, max: 10 }),
-};
+export const mockProductName = () => faker.commerce.productName();
+export const mockProductUnitPrice = () =>
+  faker.commerce.price({ min: 100, max: 1000, dec: 2 });
+export const mockProductQuantity = () => faker.number.int({ min: 1, max: 10 });
+export const mockProductCategoryName = () =>
+  getRandomString(['Basic', 'Medium', 'Premium']);
 
-export const mockUser = {
-  roleName: () => getRandomValue(['Seller', 'Client']),
-  name: () => faker.person.fullName(),
-  email: () => faker.internet.email(),
-  password: () => faker.internet.password(),
-};
+export const mockUserName = () => faker.person.fullName();
+export const mockUserEmail = () => faker.internet.email();
+export const mockUserPassword = () => faker.internet.password();
+export const mockUserRoleName = () => getRandomString(['Seller', 'Client']);
