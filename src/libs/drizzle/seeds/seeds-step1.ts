296966ac590876c67b8b3a8bf76dc4122fe7e35e
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-29 16:30:08 -0300

    seeds-step1

---
+++ src/libs/drizzle/seeds/seeds-step1.ts
@@ -0,0 +1,42 @@
+import {
+  clientDiscountsSchema,
+  productCategoriesSchema,
+  userRolesSchema,
+  usersSchema,
+} from '@/libs/drizzle/schemas';
+import { createSeed } from '@/libs/drizzle/utils';
+import {
+  mockClientDiscount,
+  mockProductCategoryName,
+  mockUserEmail,
+  mockUserName,
+  mockUserPassword,
+  mockUserRoleName,
+} from './data';
+
+// Seed definitions
+const userRoles = createSeed(userRolesSchema, {
+  name: mockUserRoleName(),
+});
+
+const users = createSeed(usersSchema, {
+  name: mockUserName(),
+  email: mockUserEmail(),
+  password: mockUserPassword(),
+});
+
+const clientDiscounts = createSeed(clientDiscountsSchema, {
+  discount: mockClientDiscount(),
+});
+
+const productCategories = createSeed(productCategoriesSchema, {
+  name: mockProductCategoryName(),
+});
+
+// Combinde and export all seeds
+export const seedsStep1 = [
+  userRoles,
+  users,
+  clientDiscounts,
+  productCategories,
+];
