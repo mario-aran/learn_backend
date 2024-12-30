ba09ae72254fb291039304909381bf0b16355d80
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-30 18:34:45 -0300

    db

--- src/libs/drizzle/db.ts
+++ src/libs/drizzle/db.ts
@@ -3,6 +3,6 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schemas from './schemas';
 
 export const db = drizzle(DATABASE_URL, {
-  schema: schemas, // All schemas to use in drizzle query
+  schema: schemas, // All schemas to use drizzle query
   logger: true, // Logs
 });
