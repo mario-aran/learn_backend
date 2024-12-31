8d34a5e8a20ae01591962aefb4d9010539b8b4ab
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-31 12:49:09 -0300

    seed-products

--- src/libs/drizzle/seeds/seed-products.ts
+++ src/libs/drizzle/seeds/seed-products.ts
@@ -3,21 +3,22 @@ import { db } from '@/libs/drizzle/db';
 import { productsSchema } from '@/libs/drizzle/schemas';
 import { faker } from '@faker-js/faker';
 
+// Requires product categories to be seeded
 export const seedProducts = async () => {
-  // Fetch all productCategorieIds
+  // Fetch all product category ids
   const productCategoryIds = await db.query.productCategoriesSchema.findMany({
     columns: { id: true },
   });
   if (!productCategoryIds?.length)
-    throw new Error('No product categories found');
+    throw new Error('No product category ids found');
 
-  // Utility to get a random productCategoryId
+  // Util to get a random product category id
   const getRandomProductCategoryId = () =>
     productCategoryIds[Math.floor(Math.random() * productCategoryIds.length)]
       .id;
 
-  // Prepare values to insert
-  const values = faker.helpers
+  // Prepare mock values
+  const mockValues = faker.helpers
     .uniqueArray(faker.commerce.productName, SEEDS_LENGTH)
     .map((name) => ({
       productCategoryId: getRandomProductCategoryId(),
@@ -25,8 +26,8 @@ export const seedProducts = async () => {
       unitPrice: faker.commerce.price({ min: 100, max: 1000, dec: 2 }),
     }));
 
-  // Insert seeds into database
-  const result = await db.insert(productsSchema).values(values);
+  // Insert seeds
+  const result = await db.insert(productsSchema).values(mockValues);
   console.log('Products seeded');
   return result;
 };
