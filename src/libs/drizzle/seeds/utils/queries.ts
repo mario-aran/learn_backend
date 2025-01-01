711ff1eed26a0850432a80ad2435232d2a3c0974
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-01 14:34:56 -0300

    queries

---
+++ src/libs/drizzle/seeds/utils/queries.ts
@@ -0,0 +1,13 @@
+import { db } from '@/libs/drizzle/db';
+import { userRolesSchema } from '@/libs/drizzle/schemas';
+import { eq } from 'drizzle-orm';
+
+export const findUserIdsByRoleName = async (roleName: string) => {
+  const userIds = await db.query.usersSchema.findMany({
+    columns: { id: true },
+    where: eq(userRolesSchema.name, roleName),
+  });
+
+  if (!userIds) throw new Error(`${roleName} user ids not found`);
+  return userIds;
+};
