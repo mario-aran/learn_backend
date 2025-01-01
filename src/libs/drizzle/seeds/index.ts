ad1774aa8795a337b7fc35da67f7ac454649306a
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-01 15:01:31 -0300

    seed orders

--- src/libs/drizzle/seeds/index.ts
+++ src/libs/drizzle/seeds/index.ts
@@ -1,5 +1,6 @@
 import { seedClients } from './seed-clients';
 import { seedInitialData } from './seed-initial-data';
+import { seedOrders } from './seed-orders';
 import { seedProducts } from './seed-products';
 import { seedSellers } from './seed-sellers';
 import { seedUsersToUserRoles } from './seed-users-to-user-roles';
@@ -11,5 +12,6 @@ const main = async () => {
   await seedUsersToUserRoles();
   await seedSellers();
   await seedClients();
+  await seedOrders();
 };
 main();
