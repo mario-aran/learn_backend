c885c4d79feaf53eb7b10d7e361412144e0f016d
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-01 17:09:24 -0300

    seeds index

--- src/libs/drizzle/seeds/index.ts
+++ src/libs/drizzle/seeds/index.ts
@@ -1,6 +1,7 @@
 import { seedClients } from './seed-clients';
 import { seedInitialData } from './seed-initial-data';
 import { seedOrders } from './seed-orders';
+import { seedOrdersToProducts } from './seed-orders-to-products';
 import { seedProducts } from './seed-products';
 import { seedSellers } from './seed-sellers';
 import { seedUsersToUserRoles } from './seed-users-to-user-roles';
@@ -8,10 +9,19 @@ import { seedUsersToUserRoles } from './seed-users-to-user-roles';
 // Insert all seeds
 const main = async () => {
   await seedInitialData();
+
+  // Requires initial data to be seeded
   await seedProducts();
   await seedUsersToUserRoles();
+
+  // Requires usersToUserRoles to be seeded
   await seedSellers();
   await seedClients();
+
+  // Requires sellers and clients to be seeded
   await seedOrders();
+
+  // Requires products and orders to be seeded
+  await seedOrdersToProducts();
 };
 main();
