f63012857d7742414fa4df7b2434e6be973c8ca7
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-03 10:15:05 -0300

    api index

--- src/app/index.ts
+++ src/app/index.ts
@@ -4,5 +4,5 @@ import express from 'express';
 const app = express();
 
 app.listen(SERVER_PORT, () => {
-  console.log(`Server is running on port: ${SERVER_PORT}`);
+  console.log(`Server running on port: ${SERVER_PORT}`);
 });
