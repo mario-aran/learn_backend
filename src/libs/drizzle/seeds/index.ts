6d0c2ae041733fd71489709d76284d3a52b11e32
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-03 18:49:09 -0300

    seed index

--- src/libs/drizzle/seeds/index.ts
+++ src/libs/drizzle/seeds/index.ts
@@ -1,33 +1,32 @@
 import { NODE_ENV } from '@/config/env';
+import { seedBaseTables } from './seed-base-tables';
+import { seedClients } from './seed-clients';
+import { seedOrders } from './seed-orders';
+import { seedOrdersToProducts } from './seed-orders-to-products';
+import { seedProducts } from './seed-products';
+import { seedSellers } from './seed-sellers';
+import { seedUsersToUserRoles } from './seed-users-to-user-roles';
 import { truncateTables } from './truncate-tables';
-import { seedClients } from './utils/seed-clients';
-import { seedInitialData } from './utils/seed-initial-data';
-import { seedOrders } from './utils/seed-orders';
-import { seedOrdersToProducts } from './utils/seed-orders-to-products';
-import { seedProducts } from './utils/seed-products';
-import { seedSellers } from './utils/seed-sellers';
-import { seedUsersToUserRoles } from './utils/seed-users-to-user-roles';
 
-// Insert all seeds
 const seedDatabase = async () => {
   // Reset tables
   await truncateTables();
 
-  // Seed data
-  await seedInitialData();
+  // Seed: no requirements
+  await seedBaseTables();
 
-  // Requires initial data to be seeded
+  // Seed: requires base tables to be seeded
   await seedProducts();
   await seedUsersToUserRoles();
 
-  // Requires usersToUserRoles to be seeded
+  // Seed: requires usersToUserRoles to be seeded
   await seedSellers();
   await seedClients();
 
-  // Requires sellers and clients to be seeded
+  // Seed: requires sellers and clients to be seeded
   await seedOrders();
 
-  // Requires products and orders to be seeded
+  // Seed: requires products and orders to be seeded
   await seedOrdersToProducts();
 };
 
