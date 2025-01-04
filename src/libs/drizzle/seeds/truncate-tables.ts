cb5381ccd026df5e48d522700c82c3302724c98a
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-04 13:39:49 -0300

    seeds truncate table

--- src/libs/drizzle/seeds/truncate-tables.ts
+++ src/libs/drizzle/seeds/truncate-tables.ts
@@ -1,9 +1,5 @@
 import { db } from '@/libs/drizzle/db';
-
-// Types
-interface SelectTablesResults {
-  rows: { table_name: string }[];
-}
+import { TX } from '@/libs/drizzle/types';
 
 // Constants
 const SELECT_TABLES_QUERY = `
@@ -12,12 +8,14 @@ const SELECT_TABLES_QUERY = `
   WHERE table_schema = 'public';
 `;
 
-export const truncateTables = async () => {
+export const truncateTables = async (tx: TX) => {
   // Fetch tables
-  const { rows }: SelectTablesResults = await db.execute(SELECT_TABLES_QUERY);
+  const { rows } = await db.execute<{ table_name: string }>(
+    SELECT_TABLES_QUERY,
+  );
   if (rows.length === 0) return console.log('No tables to truncate');
 
-  // Prepare truncate tables query
+  // Prepare truncate query
   const joinedTableNames = rows.map(({ table_name }) => table_name).join(', ');
   const truncateTablesQuery = `
     TRUNCATE TABLE ${joinedTableNames} 
@@ -27,13 +25,11 @@ export const truncateTables = async () => {
 
   try {
     // Run transaction
-    await db.transaction(async (tx) => tx.execute(truncateTablesQuery));
-
+    await tx.execute(truncateTablesQuery);
     console.log(`All tables truncated successfully: ${joinedTableNames}`);
   } catch (err) {
     // Error type assertion
     const errorMessage = err instanceof Error ? err.message : 'Unknown error';
-
     throw new Error(`Truncate tables error: ${errorMessage}`);
   }
 };
