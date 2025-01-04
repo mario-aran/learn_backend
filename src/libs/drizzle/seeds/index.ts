eb3fff2bcfaf8ac20fe705ac4e359213184d551e
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-04 13:42:35 -0300

    seed index

--- src/libs/drizzle/seeds/index.ts
+++ src/libs/drizzle/seeds/index.ts
@@ -1,4 +1,5 @@
 import { NODE_ENV } from '@/config/env';
+import { db } from '@/libs/drizzle/db';
 import { seedBaseTables } from './seed-base-tables';
 import { seedClients } from './seed-clients';
 import { seedOrders } from './seed-orders';
@@ -9,26 +10,38 @@ import { seedUsersToUserRoles } from './seed-users-to-user-roles';
 import { truncateTables } from './truncate-tables';
 
 const seedDatabase = async () => {
-  // Reset tables
-  await truncateTables();
+  try {
+    await db.transaction(async (tx) => {
+      // Reset tables
+      await truncateTables(tx);
 
-  // Seed: no requirements
-  await seedBaseTables();
+      // Seed: no requirements
+      await seedBaseTables(tx);
 
-  // Seed: requires base tables to be seeded
-  await seedProducts();
-  await seedUsersToUserRoles();
+      // Seed: requires base tables to be seeded
+      await seedProducts(tx);
+      await seedUsersToUserRoles(tx);
 
-  // Seed: requires usersToUserRoles to be seeded
-  await seedSellers();
-  await seedClients();
+      // Seed: requires usersToUserRoles to be seeded
+      await seedSellers(tx);
+      await seedClients(tx);
 
-  // Seed: requires sellers and clients to be seeded
-  await seedOrders();
+      // Seed: requires sellers and clients to be seeded
+      await seedOrders(tx);
 
-  // Seed: requires products and orders to be seeded
-  await seedOrdersToProducts();
+      // Seed: requires products and orders to be seeded
+      await seedOrdersToProducts(tx);
+    });
+
+    console.log('Database seeded successfully');
+  } catch (err) {
+    // Error type assertion
+    const errorMessage = err instanceof Error ? err.message : 'Unknown error';
+    throw new Error(`Seeding error: ${errorMessage}`);
+  }
 };
 
-// Run script only in development environment
-if (NODE_ENV !== 'production') seedDatabase();
+// Run script only in development
+if (NODE_ENV === 'production')
+  throw new Error('Seeding not allowed in production');
+seedDatabase();
