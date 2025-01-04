021b9d41b3336b38733dadd94adecf8a558ad366
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-04 16:15:55 -0300

    truncate tables

--- src/libs/drizzle/seeds/truncate-tables.ts
+++ src/libs/drizzle/seeds/truncate-tables.ts
@@ -8,12 +8,12 @@ const SELECT_TABLES_QUERY = `
   WHERE table_schema = 'public';
 `;
 
-export const truncateTables = async (tx: TX): Promise<string> => {
+export const truncateTables = async (tx: TX) => {
   // Fetch tables
   const { rows } = await db.execute<{ table_name: string }>(
     SELECT_TABLES_QUERY,
   );
-  if (rows.length === 0) return 'No tables to truncate';
+  if (rows.length === 0) return console.log('No tables to truncate');
 
   // Prepare truncate query
   const joinedTableNames = rows.map(({ table_name }) => table_name).join(', ');
@@ -25,5 +25,5 @@ export const truncateTables = async (tx: TX): Promise<string> => {
 
   // Run transaction
   await tx.execute(truncateTablesQuery);
-  return `Tables truncated successfully: ${joinedTableNames}`;
+  console.log(`Tables truncated successfully: ${joinedTableNames}`);
 };
