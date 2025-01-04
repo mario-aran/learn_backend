2ee63ac7e2163600321fb4c9daa111f3bb7d9c8a
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-04 16:43:23 -0300

    seed-products

---
+++ src/libs/drizzle/seeds/seed-products.ts
@@ -0,0 +1,33 @@
+import { SEEDS_LENGTH } from '@/libs/drizzle/constants';
+import { db } from '@/libs/drizzle/db';
+import { productsSchema, TABLE_PRODUCTS } from '@/libs/drizzle/schemas';
+import { TX } from '@/libs/drizzle/types';
+import { faker } from '@faker-js/faker';
+
+// Types
+type Product = typeof productsSchema.$inferInsert;
+
+export const seedProducts = async (tx: TX) => {
+  // Queries
+  const productCategories = await db.query.productCategoriesSchema.findMany({
+    columns: { id: true },
+  });
+  if (productCategories.length === 0)
+    throw new Error('No product categories found');
+
+  // Prepare mocks
+  const mockedProducts = faker.helpers
+    .uniqueArray(faker.commerce.productName, SEEDS_LENGTH)
+    .map(
+      (name): Product => ({
+        productCategoryId: faker.helpers.arrayElement(productCategories).id,
+        name,
+        unitPrice: faker.commerce.price({ min: 100, max: 1000, dec: 2 }),
+      }),
+    );
+
+  // Run insert transaction
+  await tx.insert(productsSchema).values(mockedProducts);
+
+  console.log(`Seeding completed for: ${TABLE_PRODUCTS}`);
+};
