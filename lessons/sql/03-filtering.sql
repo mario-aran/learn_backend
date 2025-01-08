76a44d0b6972f8f52410b2363dcfb42c84105ab5
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-08 18:39:19 -0300

    filtering

--- lessons/sql/03-filtering.sql
+++ lessons/sql/03-filtering.sql
@@ -1,12 +1,15 @@
 /* Filtering */
 
-/* `WHERE` */
+/* `WHERE`
+*/
 -- Syntax
 SELECT columns
 FROM table
 WHERE (condition); -- Optional parentheses
 
-/* Comparison Operators: `=`, `>`, `<`, `>=`, `<=`, `<>` */
+
+/* Comparison Operators: `=`, `>`, `<`, `>=`, `<=`, `<>`
+*/
 SELECT * FROM products
     WHERE unit_price = 100;
     WHERE unit_price > 100;
@@ -15,18 +18,24 @@ SELECT * FROM products
     WHERE unit_price <= 100;
     WHERE unit_price <> 100;
 
-/* Null Operators: `IS NULL`, `IS NOT NULL` */
+
+/* Null Operators: `IS NULL`, `IS NOT NULL`
+*/
 SELECT * FROM products
     WHERE unit_price IS NULL;
     WHERE unit_price IS NOT NULL;
 
-/* Logical Operators: `AND`, `OR`, `NOT` */
+
+/* Logical Operators: `AND`, `OR`, `NOT`
+*/
 SELECT * FROM products
     WHERE (unit_price > 200) AND (unit_price < 1000);
     WHERE (unit_price > 200) OR (unit_price < 1000);
     WHERE NOT unit_price > 200;
 
-/* Conditional Operators: `BETWEEN`, `IN`, `LIKE` */
+
+/* Conditional Operators: `BETWEEN`, `IN`, `LIKE`
+*/
 SELECT * FROM products
     WHERE unit_price BETWEEN 200 AND 1000;
     WHERE unit_price IN (100, 200, 300);
