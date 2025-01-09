3f375a50bce5dfb40d7cbd8bccf3b6a4db3acc39
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-08 21:16:53 -0300

    retrieving

--- lessons/sql/02-retrieving.sql
+++ lessons/sql/02-retrieving.sql
@@ -7,26 +7,22 @@ SELECT columns
 FROM table;
 
 -- All columns
-SELECT *
-FROM products;
+SELECT * FROM products;
 
 -- Specific columns, separated by commas
-SELECT id, name
-FROM products;
+SELECT id, name, unit_price FROM products;
 
 -- Combined values
-SELECT *, id, 1, 'soy una frase'
-FROM products;
+SELECT *, id, 1, 'soy una frase' FROM products;
 
 
 /* 2. Aliases: `AS`
-- Use double quotes ("") for aliases to avoid errors with reserved keywords
-- `AS` is optional
-- Can be used for columns or tables (primarily used with joins)
-- It doesn't replace the column name, it just changes its appearance
+- Use double quotes ("") for aliases to avoid errors
+- `AS` keyword is optional
+- Can be used for columns and tables(primarily with joins)
+- It doesn't replace the column name
 */
-SELECT id AS "order_id"
-FROM orders o;
+SELECT id AS "order_id" FROM orders o;
 
 
 /* 3. Arithmetic Operators: `+`, `-`, `*`, `/`, `%`
@@ -41,5 +37,4 @@ SELECT 2 * (1 + 1); -- Optional parentheses P. E. M. D. A. S.
 
 /* 4. `SELECT DISTINCT`
 */
-SELECT DISTINCT product_category_id, unit_price
-FROM products;
+SELECT DISTINCT product_category_id, unit_price FROM products;
