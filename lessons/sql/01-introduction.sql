037fa5f377921a6ff92cfe9a3c3a6670d761c23c
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-10 18:05:19 -0300

    lesson 01: introduction

--- lessons/sql/01-introduction.sql
+++ lessons/sql/01-introduction.sql
@@ -1,60 +1,47 @@
-/* Introduction */
+/* Lesson 01: Introduction */
 
-/* 1. Concepts
-*/
-- SQL: -- (Structured Query Language), allows to interact with a database.
-- DB: -- Data organized and structured in multiple tables.
-- Tables: -- Matrix of records, like excel sheets.
-- Columns: -- Fields of a table.
-- Rows: -- Records of a table.
-- Keys: -- Identify records in a table, either PK or FK.
-    - PK: -- (Primary Key), unique identifier for each record in a table.
-    - FK: -- (Foreign Key), reference to the primary key of another table.
-- ERD: -- (Entity Relationship Diagram), used to visualize the relationships between tables.
-
-/* 2. Tools
-*/
-- RDBMS: -- (Relational Database Management System), PostgreSQL, MySQL, Micorsoft SQL Server, Oracle.
-- Clients: -- DBeaver, SQL Server Management Studio, MySQL Workbench.
-- Playgrounds: -- SQL Playground.
-- Tool Settings:
-    - SQL Playground: "https://sqlplayground.app/"
-    -- Settings: "create sandbox" -> "dbms: postgresql & template: world" -> "create sandbox"
-    - "https://dbeaver.io/download/"
-    -- Connection: "new database connection" -> "all" -> "postgresql" -> "host, port, database, user, password"
-    -- Settings (UPPERCASE): "window" -> "preferences" -> "editors" -> "sql editor" -> "formatting" -> "keyword case: upper"
+/* 1. Concepts: SQL, Databases, Tables, Columns, Rows, Keys, ERD */
+- 'SQL': -- (Structured Query Language), allows to interact with a database
+- 'Databases': -- (DB), data organized and structured in multiple tables
+- 'Tables': -- Matrix of records, like excel sheets
+- 'Columns': -- Fields of a table
+- 'Rows': -- Records of a table
+- 'Keys': -- Identify records in a table (ids)
+- 'ERD': -- (Entity Relationship Diagram), used to visualize the relationships between tables
 
 
-/* 3. Syntax
-*/
-- Queries: `SELECT * FROM products;`
-- SQL is Case insensitive but:
-    UPPERCASE: -- Recommended for SQL keywords
-    snake_case: -- Recommended for everything else, like tables/columns
-- Semicolon: `;` -- Optional but recommended to define where the query ends
+/* 2. Tools: RDBMS, SQL Clients, Playgrounds */
+- 'RDBMS': -- (Relational Database Management System), PostgreSQL, MySQL, Micorsoft SQL Server, Oracle
+- 'Clients': -- DBeaver, SQL Server Management Studio, MySQL Workbench
+- 'Playgrounds': -- SQL Playground
 
-/* Comments */
--- This is a single line comment
 
-/*
-This is a multi-line comment
-in multiple lines
-*/
+/* 3. Syntax: Queries, Semicolons, Naming Conventions, Data Types, Comments */
+- 'Queries':
+    - Query Example: `SELECT * FROM products;`
+    - Query Template:
+        /* Select */
+        /* From, Join */
+        /* Where */
+        /* Group By, Having */
+        /* Order By, Limit */
+        /* Semicolon */
+- 'Semicolons': `;` -- Optional but recommended to define where the query ends
+- 'Naming Conventions': -- SQL is Case insensitive
+    - UPPERCASE: -- Recommended for SQL keywords
+    - snake_case: -- Recommended for everything else, like tables/columns
+- 'Data Types':
+    - INTEGER: 1 -- Whole numbers
+    - NUMERIC(precision, scale): 1.1 -- Decimals
+    - VARCHAR(length): 'This is text' -- Short texts
+    - TEXT: 'This is text' -- Long texts
+    - DATE: '2025-01-31' -- Dates
+    - TIMESTAMP: '2025-01-31 12:30:45' -- Dates with time
+    - BOOLEAN: true -- True or false
+- 'Comments':
+    -- This is a single line comment
 
-- Template:
-    /* Select */
-    /* From, Join */
-    /* Where */
-    /* Group By, Having */
-    /* Order By, Limit, Offset */
-    /* Semicolon */
-- Basic Data Types: Numeric, Text, Date/Time, Boolean, NULL
-
-/* Data Types */
-- INTEGER: 1
-- NUMERIC(precision, scale): 1.1
-- VARCHAR(length): 'This is text' 
-- TEXT: 'This is text'
-- DATE: '2025-01-31'
-- TIMESTAMP: '2025-01-31 12:30:45'
-- BOOLEAN: true, false
+    /*
+    This is a multi-line comment
+    in multiple lines
+    */
